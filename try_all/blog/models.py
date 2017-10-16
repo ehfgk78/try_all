4b3add25cb2b1f88e0e4eff9a24bd468e9edc405
Kim JinSeok <jskim2code@gmail.com>
2017-10-16 21:01:43 +0900

    3 ./manage startapp blog settings.py   INSTALLED_APPS = [ ~ 'blog', ] models.py   POST DB table의 모양 생성 ./manage.py makemigrations blog DB table에 Model을 반영 ./manage.py migrate blog

---
+++ try_all/blog/models.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL) # 장고걸스와 다름
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def publish(self):
+        "게시글을 발행상태로 만듦"
+        self.published_date = timezone.now()
+        self.save()
+
+    def hide(self):
+        "게시글을 미발행상태로 만듦"
+        self.published_date = None
+        self.save() 
+
+
+
